e48a466fb06ab6b7a5a304d3736c5ef47c87a656
khomson27 <76035399+khomson27@users.noreply.github.com>
2021-09-09 16:21:24 +0700

    Update Commentator.tsx

--- src/component/Commentator.tsx
+++ src/component/Commentator.tsx
@@ -13,16 +13,16 @@ const Comments = ({username, userImagePath, commentText, likeNum, replies} :Comm
         return (
             <div className='flex items-center'>
                 <img className='w-4 h-4 mr-1' src='/like.svg'></img>
-                <p className='text-gray-500'>{likeNum} คน</p>
+                <p className='text-gray-300'>{likeNum} คน</p>
             </div>
         )
     }
 
     return (
         <div>
-            <div className="flex p-2 items-start space-x-2">
+            <div className="flex p-2 items-start space-x-2" text-white>
                 <img className="w-10 w-10 rounded-full" src={userImagePath}></img>
-                <div className="bg-gray-200 rounded-lg p-2">
+                <div className="bg-gray-500 rounded-lg p-2">
                 <p className="font-semibold">{username}</p>
                 <p>{commentText}</p>
                 {/* like section (จะไม่แสดงถ้าไม่มีใครไลค์เลย) */}
